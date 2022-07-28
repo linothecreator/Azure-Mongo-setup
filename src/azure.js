2f59cabd310ca7c041ac234b0b67060952dc3c18
lino <rmbrown9520@gmail.com>
2022-07-27 17:50:52 -0700

    4

--- src/azure.js
+++ src/azure.js
@@ -7,11 +7,13 @@ var { io } = require('socket.io-client');
 const { send } = require("process");
 require('dotenv').config();
 
+//Setup and configure Socket IO
 const socket = io(process.env.WEBPAGE_URL);
 socket.on('connect', () =>{
     console.log('you connected with id: ' + socket.id);
 })
 
+var blobUrl;
 function sendURL (_url) {
     socket.emit('event', _url);
 }
@@ -19,41 +21,38 @@ function sendURL (_url) {
 //html element variables
 var selectButton = document.getElementById("selectButton");
 var _fileInput = document.getElementById("fileInput");
-var ab1=document.getElementById("ab1");
 // var dummyButton=document.getElementById("dummyButton");
 var formButton=document.getElementById("formButton");
 var form=document.getElementById("form");
 
-// ab1.value="";
-_fileInput.value="";
+//====================================================================================================
+//SETUP AN CONFIGURE AZURE BLOB API
 
 //Shared Access Signature URL
 var SASurl=process.env.SAS_URL;
+
 //lets create a blob service client
 const container=process.env.CONTAINER_NAME;
 const blobServiceClient = new BlobServiceClient(SASurl);
 const containerClient=blobServiceClient.getContainerClient(container);
 const blockBlobClient="";
 
-//lets write a function to upload an array of blobs
+//lets write a function to upload an array of blobs to azure
 function uploadBlob() {        
 console.log(_fileInput.files[0].name);
 
 //identify the file        
 const blockBlobClient = containerClient.getBlockBlobClient(_fileInput.files[0].name);
-     
-// console.log(blockBlobClient.url)    
-// blobUrl=blockBlobClient.url;     
-        
+//identify file url
+blobUrl=blockBlobClient.url; console.log(blobUrl);
+//emit url to main server.js
+sendURL(blobUrl);
+
 //upload file
-ab1.value=blockBlobClient.url;
-console.log(ab1.value);
-sendURL(ab1.value);
 blockBlobClient.uploadBrowserData(_fileInput.input);  
-// ab1.value="";
-// _fileInput.value="";
 }
 
+//====================================================================================================
 //html upload methods
 selectButton.addEventListener("click", () => _fileInput.click());
 _fileInput.addEventListener("change", uploadBlob);
